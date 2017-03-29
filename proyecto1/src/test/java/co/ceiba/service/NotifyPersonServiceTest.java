b84c27d82eae843076e4526eabc776c0696bfea6
Juan Esteban <juan.castano@ceiba.com.co>
2017-03-29 14:12:21 -0500

    GRAL:Ajuste prueba unitaria fallando

--- proyecto1/src/test/java/co/ceiba/service/NotifyPersonServiceTest.java
+++ proyecto1/src/test/java/co/ceiba/service/NotifyPersonServiceTest.java
@@ -25,6 +25,6 @@ public class NotifyPersonServiceTest {
 		//Act
 		String message = notifyPersonService.notify(person);
 		//Assert
-		Assert.assertNotNull(message);
+		Assert.assertNull(message);
 	}
 }
